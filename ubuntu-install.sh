6c34e1f6824105bb61c218489a3f9ee8c346f14e
Joose <63456252+toiviainet@users.noreply.github.com>
2020-10-26 22:56:07 +0200

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -5,37 +5,3 @@ cd ~
 ## install basic tools
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git vim zsh ssh telnet mysql-server tree ranger clang python3-pip tmux neofetch youtube-dl nodejs npm lm-sensors rtv
-
-## install `oh-my-zsh`
-cd ~/dev/dotfiles
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-cp zsh/linux-zshrc.conf ~/.zshrc
-cp zsh/joose.zsh-theme ~/.oh-my-zsh/themes/
-source ~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-source ~/.zshrc
-
-## mysql config
-
-
-## python config
-pip3 install virtualenv scipy numpy pandas jupyter tensorflow scikit-learn matplotlib seaborn pillow pyyaml requests
-
-## vim config
-cp vim/vimrc.config ~/.vimrc
-mkdir -p ~/.vim/colors/ && cp vim/colors/jellybeans.vim ~/.vim/colors/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-~/.vim/bundle/YouCompleteMe/install.py --clang-completer --tern-completer
-
-## tmux config
-git clone https://github.com/gpakosz/.tmux.git ~/.tmux
-ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
-cp ~/.tmux/.tmux.conf.local ~/.
-
-## fonts
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
