b2a2ac34a7c6d5462f3e6a69e89b73d4d8b7543c
Joose <63456252+toiviainet@users.noreply.github.com>
2020-11-07 14:49:45 +0200

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,4 +4,4 @@ cd ~
 
 ## install basic tools
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y git vim ssh mosh telnet mysql-server tree ranger inxi ansiweather clang python3-pip tmux neofetch youtube-dl nodejs npm lm-sensors rtv
+sudo apt install -y git vim ssh mosh telnet zsh mysql-server tree ranger inxi ansiweather clang python3-pip tmux neofetch youtube-dl nodejs npm lm-sensors rtv
